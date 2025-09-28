80246a282ccdbcd628cb7977563560bb0eb02a3e
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2025-09-28 16:33:40 +0530

    server added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
+// const PORT = 3000;
+console.log('Starting server...');
+
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on port http://localhost:${PORT}`);
